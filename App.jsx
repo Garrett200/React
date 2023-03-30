378e46cb10c627a4ef6e10466bce02c97ff4ff1c
Garrett <garrettjrh@live.com>
2023-03-29 23:48:08 -0600

    Started work with useState and added a function to show the modal.

--- App.jsx
+++ App.jsx
@@ -2,27 +2,35 @@ import './App.css';
 import Todo from './components/Todo.jsx'
 import Title from './components/Title.jsx'
 import Modal from './components/Modal.jsx'
+import React, { useState } from 'react';
 
 function App() {
+  
+  // showModal gives the current state and setShowModal will be the function
+  const  [showModal, setShowModal] = useState(false)
+
   return (
     <div className="App">
       <Title />
+      <div>
+        <input type="text" onChange={(event) => {
+          console.log(event.target.value)
+        }}/>
+        <button onClick={() => setShowModal(true)}>Add Todo</button>
+      </div>
       <div className="todo__wrapper">
         <Todo
-          title="Finish Frontend Simplified"
-          paragraph="Code every day for at least 30 minutes."  
+          title="Finish Frontend Simplified" 
         />
         <Todo
           title="Finish Interview Section"
-          paragraph="Get it done by April."  
         />
         <Todo
           title="Land a Job"
-          paragraph="Make at least 80k."
         />
 
       </div>
-
+      {showModal && <Modal title="Are you sure?" />}
     </div>
   );
 }
