26e080f14e9be0d4baec3b9c864df590525b5e97
Garrett <garrettjrh@live.com>
2023-04-11 08:22:38 -0600

    Added the react-router-dom library and added three routes.

--- App.jsx
+++ App.jsx
@@ -1,77 +1,15 @@
 import './App.css';
-import Todo from './components/Todo.jsx'
-import Title from './components/Title.jsx'
-import Modal from './components/Modal.jsx'
-import Counter from './components/Counter.jsx'
-import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx'
 
 function App() {
-  const [showTodo, setShowTodo] = useState([])
-  const [showModal, setShowModal] = useState(false)
-
-  function deleteTodo() {
-    setShowModal(true);
-  }
-
-  function modalConfirm() {
-    setShowModal(false);
-  }
-
-  function modalCancel() {
-    setShowModal(false);
-  }
-
-  useEffect(() => {
-    console.log('run on mount.')
-  })
-
-  useEffect(() => {
-    console.log(`run on mount and on showModal(${showModal}) change.`)
-  }, [showModal])
-
-  // Barely ever used
-  useEffect(() => {
-    console.log('run on every render.')
-  })
-
-  // showModal gives the current state and setShowModal will be the function
-
   return (
-    <div className="App">
-      <Title />
-      <div className="input__wrapper">
-        <input type="text" onChange={(event) => {
-          console.log(event.target.value)
-        }} />
-        <button>Add Todo</button>
-      </div>
-      <div className="todo__wrapper">
-        {showTodo &&
-          <Todo
-            deleteTodo={deleteTodo}
-            title="Finish Frontend Simplified"
-          />
-        }
-        {showTodo &&
-          <Todo
-            deleteTodo={deleteTodo}
-            title="Finish Interview Section"
-          />
-        }
-        {showTodo &&
-          <Todo
-            deleteTodo={deleteTodo}
-            title="Land a Job"
-          />
-        }
-
-      </div>
-      {showModal &&
-        <Modal
-          modalConfirm={modalConfirm}
-          modalCancel={modalCancel}
-          title="Are you sure?"
-        />}
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
