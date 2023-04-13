0b4cd4f0560eb29a82d6c6701802fd04b207b074
Garrett <garrettjrh@live.com>
2023-04-12 23:25:43 -0600

    Started working with Link and added a nav.

--- App.jsx
+++ App.jsx
@@ -1,13 +1,19 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home.jsx'
+import Home from './pages/Home.jsx';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Nav from './components/Nav.jsx';
 
 function App() {
   return (
     <div>
       <Router>
+        <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </div>
